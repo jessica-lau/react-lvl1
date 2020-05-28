1994cb082ddff506c96dcca80a67c57bcea243f1
jessica-lau <jessica.lau2@gmail.com>
2020-05-28 18:18:29 -0400

    fixed linking between files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello, my name is Jessica</h1>
+      <h1>Hello, my name is Jessica!</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
